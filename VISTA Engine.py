fc577cc55befbc8e20025f669295055cb0cc2603
Prathmaya <prathmaya.848@gmail.com>
2026-06-20 19:57:09 +0530

    Updated VISTA Engine

--- VISTA Engine.py
+++ VISTA Engine.py
@@ -7,14 +7,6 @@ import time
 import socket
 import ctypes
 import threading
-import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
 
 if getattr(sys, 'frozen', False):
     base_dir = os.path.expanduser('~')
